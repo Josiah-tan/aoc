de4e725bb61f647ecfd997381f035738b55cd0af
josiah <josiah123t@gmail.com>
2023-12-02 02:01:01 +1100

    linear time Aho Corasick solution

--- 2023/day_1/main.cpp
+++ 2023/day_1/main.cpp
@@ -12,113 +12,137 @@
 using namespace std;
 
 class AhoCorasick {
-private:
-    struct Node {
-        std::unordered_map<char, int> children;
-        int link;
-        int value;
+	private:
+		struct Node {
+			std::unordered_map<char, int> children;
+			int link;
+			int value;
 
-        Node() : link(-1), value(-1) {}
-    };
+			Node() : link(-1), value(-1) {}
+		};
 
-    std::vector<Node> trie;
+		std::vector<Node> trie;
 
-public:
-    AhoCorasick();
-    void insert(const std::string& key, int value);
-    void build();
-    int query(const std::string& text);
+	public:
+		int sum;
+		AhoCorasick();
+		void insert(const std::string& key, int value);
+		void build();
+		void query(const std::string& text);
 };
 
 AhoCorasick::AhoCorasick() {
-    trie.emplace_back(); // Root node
+	trie.emplace_back(); // Root node
+	sum = 0;
 }
 
 void AhoCorasick::insert(const std::string& key, int value) {
-    int current = 0; // Start from the root
+	int current = 0; // Start from the root
 
-    for (char ch : key) {
-        if (trie[current].children.find(ch) == trie[current].children.end()) {
-            trie.emplace_back();
-            trie[current].children[ch] = trie.size() - 1;
-        }
-        current = trie[current].children[ch];
-    }
+	for (char ch : key) {
+		if (trie[current].children.find(ch) == trie[current].children.end()) {
+			trie.emplace_back();
+			trie[current].children[ch] = trie.size() - 1;
+		}
+		current = trie[current].children[ch];
+	}
 
-    trie[current].value = value;
+	trie[current].value = value;
 }
 
 void AhoCorasick::build() {
-    std::queue<int> bfsQueue;
+	std::queue<int> bfsQueue;
 
-    for (const auto& child : trie[0].children) {
-        int next = child.second;
-        trie[next].link = 0;
-        bfsQueue.push(next);
-    }
+	for (const auto& child : trie[0].children) {
+		int next = child.second;
+		trie[next].link = 0;
+		bfsQueue.push(next);
+	}
 
-    while (!bfsQueue.empty()) {
-        int current = bfsQueue.front();
-        bfsQueue.pop();
+	while (!bfsQueue.empty()) {
+		int current = bfsQueue.front();
+		bfsQueue.pop();
 
-        for (const auto& child : trie[current].children) {
-            char ch = child.first;
-            int next = child.second;
-            bfsQueue.push(next);
+		for (const auto& child : trie[current].children) {
+			char ch = child.first;
+			int next = child.second;
+			bfsQueue.push(next);
 
-            int link = trie[current].link;
-            while (link != -1 && trie[link].children.find(ch) == trie[link].children.end()) {
-                link = trie[link].link;
-            }
+			int link = trie[current].link;
+			while (link != -1 && trie[link].children.find(ch) == trie[link].children.end()) {
+				link = trie[link].link;
+			}
 
-            trie[next].link = (link == -1) ? 0 : trie[link].children[ch];
-        }
-    }
+			trie[next].link = (link == -1) ? 0 : trie[link].children[ch];
+		}
+	}
 }
 
-int AhoCorasick::query(const std::string& text) {
-    int current = 0;
-    int result = 0;
+void AhoCorasick::query(const std::string& text) {
+	int current = 0;
+	int first = -1;
+	int last = -1;
 
-    for (char ch : text) {
-        while (current != 0 && trie[current].children.find(ch) == trie[current].children.end()) {
-            current = trie[current].link;
-        }
+	for (char ch : text) {
+		while (current != 0 && trie[current].children.find(ch) == trie[current].children.end()) {
+			current = trie[current].link;
+		}
 
-        if (trie[current].children.find(ch) != trie[current].children.end()) {
-            current = trie[current].children[ch];
-        }
+		if (trie[current].children.find(ch) != trie[current].children.end()) {
+			current = trie[current].children[ch];
+		}
 
-        int match = current;
+		int match = current;
 
-        while (match != 0) {
-            if (trie[match].value != -1) {
-                // Found a match, add the associated value to the result
-                result += trie[match].value;
-            }
-            match = trie[match].link;
-        }
-    }
-
-    return result;
+		while (match != 0) {
+			if (trie[match].value != -1) {
+				// Found a match, add the associated value to the result
+				int result = trie[match].value;
+				if (first == -1) first = result;
+				last = result;
+			}
+			match = trie[match].link;
+		}
+	}
+	sum += first * 10 + last;
+	// return result;
 }
 
 int main() {
-    AhoCorasick ac;
+	AhoCorasick ac;
 
-    // Insert keys and values
-    ac.insert("abc", 5);
-    ac.insert("123", 2);
-    ac.insert("def", 3);
+	// Insert keys and values
+	ac.insert("1", 1);
+	ac.insert("2", 2);
+	ac.insert("3", 3);
+	ac.insert("4", 4);
+	ac.insert("5", 5);
+	ac.insert("6", 6);
+	ac.insert("7", 7);
+	ac.insert("8", 8);
+	ac.insert("9", 9);
 
-    // Build the Aho-Corasick trie
-    ac.build();
+	bool part_2 = true;
+	if (part_2){
+		ac.insert("one", 1);
+		ac.insert("two", 2);
+		ac.insert("three", 3);
+		ac.insert("four", 4);
+		ac.insert("five", 5);
+		ac.insert("six", 6);
+		ac.insert("seven", 7);
+		ac.insert("eight", 8);
+		ac.insert("nine", 9);
+	}
 
-    // Query text and print the result
-    std::string text = "abc123def";
-    int result = ac.query(text);
+	// Build the Aho-Corasick trie
+	ac.build();
 
-    std::cout << "Result: " << result << std::endl;
-
-    return 0;
+	// Query text and print the result
+	std::string text;
+	while(getline(cin, text)){
+		ac.query(text);
+	}
+	cout << ac.sum;
+	return 0;
 }
